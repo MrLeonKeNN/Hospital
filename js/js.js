074b109733633b23ef24a0c3fae078ec36fac21d
MrLeonKeNN <61803026+MrLeonKeNN@users.noreply.github.com>
2020-10-30 21:20:58 +0300

    !

--- js/js.js
+++ js/js.js
@@ -1,5 +1,24 @@
-$('#myModal').on('shown.bs.modal', function () {
-	$('#myInput').trigger('focus')
+let a;
+let b = document.getElementById("block3");
+window.addEventListener('scroll', function() {
+	document.getElementById('scroll').innerHTML = pageYOffset + 'px';
+	 a =document.getElementById('scroll');
+	 
+	 console.log(pageYOffset)
   });
- // alert("hello");
+  console.log(b)
 
+  function zeroOne(){
+
+		b.classList.add("addmargin");
+
+  }
+  //document.addEventListener("DOMContentLoaded",zeroOne);
+setTimeout(zeroOne,2);
+
+
+
+
+
+
+// onclick.menuToggle = zero();
