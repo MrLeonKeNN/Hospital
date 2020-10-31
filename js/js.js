87ff7655ebed7638ebbd499e4f70a74ada3ccf84
MrLeonKeNN <61803026+MrLeonKeNN@users.noreply.github.com>
2020-10-31 21:59:15 +0300

    end?

--- js/js.js
+++ js/js.js
@@ -7,21 +7,35 @@ let div1 = document.getElementById("block4_1");
 let div2 = document.getElementById("block4_2");
 let div3 = document.getElementById("block5_div");
 let div4 = document.getElementById("div_is_one");
+let div5 = document.getElementById("div_is_two");
+let div6 = document.getElementById("two_div");
+let div7 = document.getElementById("tree_div");
+let div8 = document.getElementById("four_div") ;
+let div9 = document.getElementById("five_div");
 window.addEventListener('scroll', function() {
 
-	 a =document.getElementById('scroll');
+   a =document.getElementById('scroll');
+   if(pageYOffset>=200){
+    div1.classList.remove("remmarginLeft");
+    div2.classList.remove("remmarginRight");
+  }
    if(pageYOffset>=500){
     direct.classList.add("addmarginLeft")
   }
     console.log("a=",pageYOffset);
-    if(pageYOffset>=200){
-      div1.classList.remove("remmarginLeft");
-      div2.classList.remove("remmarginRight");
-    }
-    if(pageYOffset>=800){
-      div3.style.marginRight="0px";
     
+    if(pageYOffset>=800){
+       div3.classList.remove("remmarginRight2");
     }
+    if(pageYOffset>=2300){
+      div6.classList.remove("remmarginLeft");
+   
+    }
+    if(pageYOffset>=3150){
+      div8.classList.remove("remmarginLeft3");
+      div9.classList.remove("remmarginRight3");
+    }
+    
    
     console.log(pageYOffset);
   });
